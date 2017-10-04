a5e6828b94ddc359ee541bd4fe0bcedbefd8d801
Siddharth Panigrahi <siddharth7997@gmail.com>
2017-10-04 21:47:16 +0530

    Delete Test.py

--- Desktop/Test.py
+++
@@ -1,6 +0,0 @@
-import feedparser
-
-d = feedparser.parse('https://www.google.com/finance/company_news?q=NASDAQ:MSFT&ei=CLh5WfiQF4KKuwTa47i4CQ&output=rss&num=100')
-
-for i in range(100):
-	print i,d['entries'][i]['title']
